c2d5820f046b19df7d23a023e3882d4eaad0e5c9
jekovniki <jekovniki95@gmail.com>
2024-05-06 09:41:52 +0300

    Add theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
 import './App.css'
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider } from './components/theme-provider';
 import { TRoutes } from './types/base';
 import { routes } from './pages/router';
+import { THEME_STORAGE_KEY } from './utils/constants';
 
 function App() {
   const queryClient = new QueryClient();
 
   const setRoutes = (routes: TRoutes[]) => {
-    return routes.map(({route, component, key}) => <Route path={route} element={component} key={key} />)
+    return routes.map(({ route, component, key }) => <Route path={route} element={component} key={key} />)
   }
   return (
-    <QueryClientProvider client={queryClient}>
-      <Routes>
-        {setRoutes(routes)}
-        <Route path="*" element={<Navigate to="/404" />} />
-      </Routes>
-    </QueryClientProvider>
+    <ThemeProvider defaultTheme='light' storageKey={THEME_STORAGE_KEY}>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          {setRoutes(routes)}
+          <Route path="*" element={<Navigate to="/404" />} />
+        </Routes>
+      </QueryClientProvider>
+    </ThemeProvider>
+
   )
 }
 
