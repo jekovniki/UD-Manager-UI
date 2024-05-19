98365833db0a4e10e172a225ebca2206e6952e18
jekovniki <jekovniki95@gmail.com>
2024-05-19 22:30:27 +0300

    Move the bell

--- src/layouts/platform.tsx
+++ src/layouts/platform.tsx
@@ -30,10 +30,6 @@ const PlatformLayout = () => {
                             <Package2 className="h-6 w-6" />
                             <span className="">Acme Inc</span>
                         </Link>
-                        <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
-                            <Bell className="h-4 w-4" />
-                            <span className="sr-only">Toggle notifications</span>
-                        </Button>
                     </div>
                     <div className="flex-1">
                         <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
@@ -154,6 +150,10 @@ const PlatformLayout = () => {
                             </div>
                         </form>
                     </div>
+                    <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
+                        <Bell className="h-4 w-4" />
+                        <span className="sr-only">Toggle notifications</span>
+                    </Button>
                     <DropdownMenu>
                         <DropdownMenuTrigger asChild>
                             <Button variant="secondary" size="icon" className="rounded-full">
