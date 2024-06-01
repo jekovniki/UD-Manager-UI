7fbc4674d4cd868c51fc762b8fca4bc06fcb01f2
jekovniki <jekovniki95@gmail.com>
2024-06-01 19:26:34 +0300

    feat(obligations-calendar): make the listing appropriate

--- src/app/obligations/components/item.tsx
+++ src/app/obligations/components/item.tsx
@@ -9,10 +9,10 @@ export type ObligationListItem = {
     createdBy: string
 }
 
-export const ObligationItem = ({ item, severity }: { item: ObligationListItem, severity: ObligationSeverity }) => {
+export const ObligationItem = ({ item, severity, ...props }: { item: ObligationListItem, severity: ObligationSeverity }) => {
     const date = new Date(item.date);
     return (
-        <li className="mb-4 flex gap-2 items-center">
+        <li className="mb-4 flex gap-2 items-center" {...props}>
             <div className={severity + ' rounded h-[45px] w-[50px] text-primary-foreground flex flex-col items-center'}>
                 <span className="font-medium text-lg mt-[1px]">{format(date, 'dd')}</span>
                 <span className="capitalize text-xs font-light mt-[-5px]">
--- src/app/obligations/components/list.tsx
+++ src/app/obligations/components/list.tsx
@@ -1,55 +1,42 @@
 import { ObligationItem, ObligationListItem } from "./item"
 import { ObligationSeverity } from "@/enums/obligations"
 import { addDays, isToday, isTomorrow, isWithinInterval, intlFormat } from 'date-fns';
-import _, { findIndex, groupBy } from 'lodash';
+import { groupBy } from 'lodash';
 
 export const ObligationsList = ({ list = [] }: { list: ObligationListItem[] }) => {
-    const filteredList = [...list];
+    // const filteredList = _.groupBy(list, (obligation) => {
+    //     return obligation.date.split('T')[0];
+    // });
+    const today = new Date();
+    const twoDaysFromToday = addDays(today, 2);
+
     const critical = list.filter(item => isToday(item.date));
     const danger = list.filter(item => isTomorrow(item.date));
-    const warning = list.filter(item => {
-        const today = new Date();
-        const twoDaysFromToday = addDays(today, 2);
-        
-        return isWithinInterval(new Date(item.date), { start: today, end: twoDaysFromToday });
+    const warning = list.filter(item => isWithinInterval(new Date(item.date), { start: today, end: twoDaysFromToday }));
+
+    const otherDates = list.filter(item => {
+        return !isToday(new Date(item.date)) && !isTomorrow(new Date(item.date)) && !isWithinInterval(new Date(item.date), { start: twoDaysFromToday, end: twoDaysFromToday });
     });
 
-    critical.forEach(item => {
-        const index = findIndex(filteredList, (listItem) => listItem.id === item.id);
-        if (index !== -1) {
-          filteredList.splice(index, 1);
-        }
-      });
-    
-      danger.forEach(item => {
-        const index = findIndex(filteredList, (listItem) => listItem.id === item.id);
-        if (index !== -1) {
-          filteredList.splice(index, 1);
-        }
-      });
-    
-      warning.forEach(item => {
-        const index = findIndex(filteredList, (listItem) => listItem.id === item.id);
-        if (index !== -1) {
-          filteredList.splice(index, 1);
-        }
-      });
-      const info = groupBy(filteredList.filter(item => item.date), (item) => item.id);
+    const info = groupBy(otherDates, (obligation) => {
+        return obligation.date.split('T')[0];
+    });
+
     return (
         <>
             { critical.length ?
                 <>
                     <h5 className="label-color text-sm mt-4 mb-2 font-light mb-none">Днес</h5>
-                    <ul>
-                        { critical.map(item => <ObligationItem key={item.id} item={item} severity={ObligationSeverity.Critical} />) }
+                    <ul key='ul-critical'>
+                        { critical.map(item => <ObligationItem key={`critical-${item.id}`} item={item} severity={ObligationSeverity.Critical} />) }
                     </ul>
                 </> : ""
             }
             { danger.length ?
                 <>
                     <h5 className="label-color text-sm mt-4 mb-2 font-light">Утре</h5>
-                    <ul>
-                        { danger.map(item => <ObligationItem key={item.id} item={item} severity={ObligationSeverity.Danger} />) }
+                    <ul key={'ul-danger'}>
+                        { danger.map(item => <ObligationItem key={`danger-${item.id}`} item={item} severity={ObligationSeverity.Danger} />) }
                     </ul>
                 </> : ""
             }
@@ -59,28 +46,24 @@ export const ObligationsList = ({ list = [] }: { list: ObligationListItem[] }) =
                     {
                         locale: 'bg-BG'
                     })}</h5>
-                    <ul>
-                        { warning.map(item => <ObligationItem key={item.id} item={item} severity={ObligationSeverity.Warning} />) }
+                    <ul key={'ul-warning'}>
+                        { warning.map(item => <ObligationItem key={`warning-${item.id}`} item={item} severity={ObligationSeverity.Warning} />) }
                     </ul>
                 </> : ""
             }
-            { info.toString() !== '{}' ? Object.keys(info).map((value: string, index: number) => {
-                console.log('value', value);
-                console.log('index', index);
-                console.log('array : ', info[index]);
-                console.log('sub - : ', info[index]?.[0]);
-                return (<>
-                    <h5 className="label-color text-sm mt-4 mb-2 font-light">{intlFormat(info[index]?.[0].date, { year: 'numeric', month: 'long', day: 'numeric' }, 
-                    {
-                        locale: 'bg-BG'
-                    })}</h5>
-                    <ul>
-                        { info[index].map((item) => <ObligationItem key={item.id} item={item} severity={ObligationSeverity.Info} />) }
-                    </ul>
-                </>)
+            { Object.keys(info).length ? Object.values(info).map((value, index) => {
+                return (
+                    <div key={`info-${index}`}>
+                        <h5 className="label-color text-sm mt-4 mb-2 font-light" key={index}>{intlFormat(new Date(value[0].date), { year: 'numeric', month: 'long', day: 'numeric' }, 
+                        {
+                            locale: 'bg-BG'
+                        })}</h5>
+                        <ul key={`ul-info-${index}`}>
+                            { value.map((item) => <ObligationItem key={`info-${item.id}`} item={item} severity={ObligationSeverity.Info} />) }
+                        </ul>
+                    </div>
+                )
             }) : ""
-
-                
             }
 
         </>
