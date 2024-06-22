be089b6f160dcd11b39786901b1acb8c2c0d0f5f
jekovniki <jekovniki95@gmail.com>
2024-06-22 11:53:28 +0300

    fix filtering

--- src/app/authentication/api/use-company-register.ts
+++ src/app/authentication/api/use-company-register.ts
@@ -1,20 +1,27 @@
-import { InvalidateQueryFilters, useMutation, useQueryClient } from "@tanstack/react-query";
+import {
+	InvalidateQueryFilters,
+	useMutation,
+	useQueryClient,
+} from "@tanstack/react-query";
 import { AuthenticationQueryKeys } from "./query-keys";
 
 const mockApi = (data: unknown): Promise<{ data: string }> => {
-    console.log('mockApi data: ', data);
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve({ data: 'Hello world!' })
-        }, 5000);
-    });
-}
+	console.log("mockApi data: ", data);
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve({ data: "Hello world!" });
+		}, 5000);
+	});
+};
 
 export function useCompanyRegister() {
-    const client = useQueryClient();
+	const client = useQueryClient();
 
-    return useMutation({
-        mutationFn: mockApi,
-        onSuccess: () => client.invalidateQueries(AuthenticationQueryKeys.CompanyRegister as InvalidateQueryFilters)
-    })
+	return useMutation({
+		mutationFn: mockApi,
+		onSuccess: () =>
+			client.invalidateQueries(
+				AuthenticationQueryKeys.CompanyRegister as InvalidateQueryFilters,
+			),
+	});
 }
