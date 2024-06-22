fd9fd55fc1c1464640d5550db6cdece4d3ae02b2
jekovniki <jekovniki95@gmail.com>
2024-06-22 11:45:24 +0300

    feat/prettier config prettier

--- src/pages/user-register.tsx
+++ src/pages/user-register.tsx
@@ -2,15 +2,15 @@ import { UserRegisterForm } from "@/app/authentication/components/user-register-
 import { Card } from "@/components/ui/card";
 
 const UserRegister = () => {
-    return (
-        <main className="min-h-screen flex items-center p-4">
-            <Card className="mx-auto max-w-[37.75rem] w-full rounded-sm shadow-2xl border-transparent">
-                <div className="mt-4 w-full sm:mx-auto">
-                    <UserRegisterForm />
-                </div>
-            </Card>
-        </main>
-    );
-}
+	return (
+		<main className="min-h-screen flex items-center p-4">
+			<Card className="mx-auto max-w-[37.75rem] w-full rounded-sm shadow-2xl border-transparent">
+				<div className="mt-4 w-full sm:mx-auto">
+					<UserRegisterForm />
+				</div>
+			</Card>
+		</main>
+	);
+};
 
 export default UserRegister;
